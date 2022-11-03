6605f104690f76e979fa36f76b4f887e2346f90f
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-11-03 15:13:54 +0600

    category

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require("express")
 const cors = require("cors")
 
+
+const products = require("./products");
+
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+
+app.get("/products", (req, res) =>{
+    res.send(products);
+});
+
+
+
+
+
+
+
+app.get("/", (req, res) =>{
+    res.send("Welcome to Shopping Cart...");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, console.log(`Server is running on port ${port}`));
---
+++ products.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+      id: 1,
+      name: "Chicken Burger",
+      category: "burger",
+      desc: "",
+      price: 999,
+      image:
+        "https://i.ibb.co/r46XfkQ/burger1.png"
+        ,
+    },
+    {
+      id: 2,
+      name: "Vegitable Pizza",
+      category: "pizza",
+      desc: "",
+      price: 699,
+      image:
+        "https://i.ibb.co/n10hw2F/pizza1.jpg",
+    },
+    {
+      id: 3,
+      name: "Veg Fried Rice",
+      category: "fried rice",
+      desc: "",
+      price: 399,
+      image:
+        "https://i.ibb.co/ryfgsdX/rice1.webp",
+    },
+  ];
+  
+  module.exports = products;
